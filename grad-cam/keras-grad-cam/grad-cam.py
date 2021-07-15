2b615d4d73249455894d9b40e8bcc379073c64f0
matthew.wang@darwinai.ca <mslwang2001@gmail.com>
2021-07-15 14:12:28 -0400

    Remove guided backprop code from grad-cam

--- grad-cam/keras-grad-cam/grad-cam.py
+++ grad-cam/keras-grad-cam/grad-cam.py
@@ -34,75 +34,12 @@ def load_image(path):
     return x
 
 
-def register_gradient():
-    if "GuidedBackProp" not in ops._gradient_registry._registry:
-
-        @ops.RegisterGradient("GuidedBackProp")
-        def _GuidedBackProp(op, grad):
-            dtype = op.inputs[0].dtype
-            return grad * tf.cast(grad > 0., dtype) * \
-                tf.cast(op.inputs[0] > 0., dtype)
-
-
-def compile_saliency_function(model, activation_layer='block5_conv3'):
-    input_img = model.input
-    layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
-    layer_output = layer_dict[activation_layer].output
-    max_output = K.max(layer_output, axis=3)
-    saliency = K.gradients(K.sum(max_output), input_img)[0]
-    return K.function([input_img, K.learning_phase()], [saliency])
-
-
-def modify_backprop(model, name):
-    g = tf.get_default_graph()
-    with g.gradient_override_map({'Relu': name}):
-
-        # get layers that have an activation
-        layer_dict = [
-            layer for layer in model.layers[1:]
-            if hasattr(layer, 'activation')
-        ]
-
-        # replace relu activation
-        for layer in layer_dict:
-            if layer.activation == keras.activations.relu:
-                layer.activation = tf.nn.relu
-
-        # re-instanciate a new model
-        new_model = VGG16(weights='imagenet')
-    return new_model
-
-
 def _compute_gradients(tensor, var_list):
     grads = tf.gradients(tensor, var_list)
     return [
         grad if grad is not None else tf.zeros_like(var)
         for var, grad in zip(var_list, grads)
     ]
-
-
-def deprocess_image(x):
-    '''
-    Same normalization as in:
-    https://github.com/fchollet/keras/blob/master/examples/conv_filter_visualization.py
-    '''
-    if np.ndim(x) > 3:
-        x = np.squeeze(x)
-    # normalize tensor: center on 0., ensure std is 0.1
-    x -= x.mean()
-    x /= (x.std() + 1e-5)
-    x *= 0.1
-
-    # clip to [0, 1]
-    x += 0.5
-    x = np.clip(x, 0, 1)
-
-    # convert to RGB array
-    x *= 255
-    if K.image_dim_ordering() == 'th':
-        x = x.transpose((1, 2, 0))
-    x = np.clip(x, 0, 255).astype('uint8')
-    return x
 
 
 def grad_cam(input_model, image, category_index, layer_name):
@@ -174,10 +111,3 @@ print(f"predicted_class: {predicted_class}")
 cam, heatmap = grad_cam(model, preprocessed_input, predicted_class,
                         "block5_conv3")
 cv2.imwrite("gradcam.jpg", cam)
-
-register_gradient()
-guided_model = modify_backprop(model, 'GuidedBackProp')
-saliency_fn = compile_saliency_function(guided_model)
-saliency = saliency_fn([preprocessed_input, 0])
-gradcam = saliency[0] * heatmap[..., np.newaxis]
-cv2.imwrite("guided_gradcam.jpg", deprocess_image(gradcam))
