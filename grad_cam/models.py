c34d0ee851056504d49386e9087dec6b3c7d0932
matthew.wang@darwinai.ca <mslwang2001@gmail.com>
2021-08-05 14:07:48 -0400

    Convert Model class to abstract base class

--- grad_cam/models.py
+++ grad_cam/models.py
@@ -2,8 +2,10 @@ import tensorflow as tf
 import numpy as np
 import os
 
+from abc import ABC, abstractmethod
 
-class Model:
+
+class Model(ABC):
     def __init__(self, args):
         self.args = args
         tf.reset_default_graph()
@@ -18,6 +20,22 @@ class Model:
 
     def get_session(self):
         return self.sess
+
+    def get_conv_output_tensor(self):
+        graph = tf.get_default_graph()
+        return graph.get_tensor_by_name(self.args.last_conv_out_tensor)
+
+    @abstractmethod
+    def get_loss_op(self):
+        pass
+
+    @abstractmethod
+    def get_feed_dict(self, preprocessed_input, one_hot):
+        pass
+
+    @abstractmethod
+    def get_one_hot(self, prediction_matrix):
+        pass
 
     def __del__(self):
         self.sess.close()
@@ -43,10 +61,6 @@ class Covidnet(Model):
             self.args.out_logit_tensor)
         return self.get_covidnet_loss(pred_logit_tensor, labels_tensor,
                                       sample_weights)
-
-    def get_conv_output_tensor(self):
-        graph = tf.get_default_graph()
-        return graph.get_tensor_by_name(self.args.last_conv_out_tensor)
 
     def get_feed_dict(self, preprocessed_input, one_hot):
         graph = tf.get_default_graph()
